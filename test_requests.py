111661841df239d2f97d9c4636ce2189ae982b2c
Puttipong1234 <Puttipong.lims@gmail.com>
2023-05-25 23:40:02 +0700

    fix pass

--- test_requests.py
+++ test_requests.py
@@ -2,14 +2,14 @@ import requests
 import json
 
 url = 'http://127.0.0.1:5000/signals'
-heroku_url = "https://my-bot-trade-only-eu.herokuapp.com/signals" # webhook
+heroku_url = "https://xxxxxxx.com/signals" # webhook
 ข้อมูลตัวอย่าง = {
             'ACTION': 'OPEN SHORT', # << USE
             'AMOUNT_COIN' : '0.1', # << USE
             'AMOUNT_USDT' : '30.00',
             'LEV' : '[PB-BTC-06]', 
             'SYMBOL' : 'BTCBUSD', # << USE
-            'PASSWORD': "1100801127618", # << USE
+            'PASSWORD': "xxxxx", # << USE
             'FACTOR' : "10" # << USE....
             }
 
